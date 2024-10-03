536f938022b15e4e063ba8be54f984a212beecc0
ollysheriblakollysheriblak <ollysheriblakegedicvpymssed@hotmail.com>
2024-10-03 09:29:05 +0000

    Initialised

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("","10fc69a6f9c1869ab552b728ce395a63ce25450cf42874bd6bdfda1ecac7a63e"){} 
+    constructor()ERC20("v","f53ae727b3e49c47c1146a0b683530eb1a9aefadcbed3820f0f9b85b6e45331d"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
